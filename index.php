f7c34949bfab89dd0953c62dd80b82b500832ab8
Danunuch Kongwatmai <71056612+Danunuch@users.noreply.github.com>
2021-01-11 19:16:30 +0700

    re

--- index.php
+++ index.php
@@ -2,30 +2,36 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WebDanunuch</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <script src="js/default.min.js"></script>
-    <script src="JS/jquery-3.5.1.min.js"></script>
-
-    <link rel="stylesheet" href="Icon/css/all.css">
-
-    <link rel="stylesheet" href="css/mystyle.css">
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <link rel="stylesheet" href="css/Mystyle.css">
     <script src="js/default.js"></script>
-    
+    <link rel="stylesheet" href="icon/css/all.css">
 </head>
 <body>
     <div class="container">
+        <center>
+        <h1>Danunuch</h1><br>
+        <img src="IMG/Profile.jpg" width="350px" alt="" srcset="">
+         <br>
+        <br>
+        <button class='btn btn-primary'><i class="fab fa-algolia"></i></i></button>
+        <button class='btn btn-primary'><i class="fas fa-anchor"></i></i></i></button>      
+        <button class='btn btn-primary'><i class="fab fa-apple"></i></i></button>
+        <button class='btn btn-primary'><i class="fas fa-archway"></i></i></i></button>
+            <br>
+        </center>
 
+        
 
-    <button class="btn btn-primary"><i class="fab fa-accessible-icon"></i></button> 
-    <button class="btn btn-primary"><i class="fas fa-cheese"> Cheese</i></button> 
-   
-</div>
-<script>
-  $(()=>{
-  //      alert("Hello");
-    });
-</script>
+    </div>
+    <script>
+        $(()=>{
+           // alert("Hello");
+        })
+    </script>
 </body>
 </html>
